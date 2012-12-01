9f848562016ab7704e295c14871cf087a22cf355
Itamar Reis Peixoto <itamar@ispbrasil.com.br>
2012-12-01 20:13:07 -0200

    ported schema to postgresql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name CHAR(100) NOT NULL,
-    closed BOOL NOT NULL
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id serial NOT NULL PRIMARY KEY,
+    name character(100) NOT NULL,
+    closed boolean NOT NULL
 );
 
-INSERT OR IGNORE INTO tasks (id, name, closed) VALUES (1, 'Start learning Pyramid', 0);
-INSERT OR IGNORE INTO tasks (id, name, closed) VALUES (2, 'Do quick tutorial', 0);
-INSERT OR IGNORE INTO tasks (id, name, closed) VALUES (3, 'Have some beer!', 0);
 
+INSERT INTO tasks (id, name, closed) VALUES (1, 'Start learningPyramid', false);
+INSERT INTO tasks (id, name, closed) VALUES (2, 'Do quick tutorial', false);
+INSERT INTO tasks (id, name, closed) VALUES (3, 'Have some beer!', false);
